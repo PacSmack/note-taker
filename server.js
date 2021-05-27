7e58221ca2b574d27586b9a80b562b1fef8c136d
Nathan da Silva <pacsmacck@gmail.com>
2021-05-27 12:41:33 -0700

    succesfully retrieving notes json and displaying on page

--- server.js
+++ server.js
@@ -1,14 +1,26 @@
 const express = require('express');
 const { notes } = require('./db/db.json');
 
+const fs = require('fs');
+const path = require('path');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.static("public"));
 
-app.use(express.static("public"))
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'))
+})
 
 app.get("/api/notes", (req, res) => {
     res.json(notes);
-});
+})
 
 
 
